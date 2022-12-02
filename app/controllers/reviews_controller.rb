8b0fa1ae580a710a683d97bdcd43423480ddfc5a
Meshack <mbuvi.meshack99@gmail.com>
2022-12-02 10:44:28 +0300

    commit tree 111318a53ecd3261e40b16b80c9e531d52c0776e

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,4 +26,27 @@ class ReviewsController < ApplicationController
     params.permit(:username, :comment, :rating)
   end
 
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
+
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
+
+  end
+
 end
